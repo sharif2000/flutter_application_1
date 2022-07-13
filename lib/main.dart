213107804e3c5ad5e2825d8db39ab48a4fa08d79
sharif2000 <sharif_pc@hotmail.com>
2022-07-13 16:01:51 +0200

    first

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors, annotate_overrides, use_key_in_widget_constructors
+import 'package:flutter/material.dart';
+
+main() => runApp(App0301());
+
+class App0301 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Text("Hello world!"),
+      ),
+    );
+  }
+}
