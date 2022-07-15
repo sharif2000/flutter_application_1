df681c8c6c5026f60b1cd4a36ae055df82d1d0de
sharif2000 <sharif_pc@hotmail.com>
2022-07-15 11:40:23 +0200

    End of centering text on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
-// ignore_for_file: prefer_const_constructors, annotate_overrides, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors, annotate_overrides, use_key_in_widget_constructors, sort_child_properties_last, deprecated_member_use, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 
-main() => runApp(App0301());
+main() => runApp(MyApp());
 
-class App0301 extends StatelessWidget {
+class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Text("Hello world!"),
-      ),
+      home: Scaffold(
+        appBar: AppBar(title: (Text("Quarrtsi", style: TextStyle(fontSize: 26.0)))),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, 
+            children: [Text("We Recommend", style: TextStyle(fontSize: 26.0)),
+            Text("It's lonely for me inside this phone.",style: TextStyle(fontSize: 26.0)),]
+          ),
+        ),
+        drawer: Drawer(child: Center(child: Text("Settings will be here"),),),
+      )
     );
   }
 }
