c2c3c3cdf750a8f9eff687ed4f1c6a0cca2cfa34
sharif2000 <sharif_pc@hotmail.com>
2022-07-24 18:35:08 +0200

    added "named routes"

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, unused_local_variable, annotate_overrides
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, unused_local_variable, annotate_overrides, non_constant_identifier_names
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/search.dart';
 
 main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+              '/search': (context) => Search(),
+      },
       home: Scaffold(
         body: Stack(children:[
           TheBackground(),
@@ -30,12 +34,11 @@ class TheNextButton extends StatelessWidget {
         primary: Colors.yellow.shade300,
         onPrimary: Colors.black,
         textStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.bold));
-
   Widget build(BuildContext context) {
     return Center(child:
       Column(mainAxisAlignment: MainAxisAlignment.end, children:[
         Padding(padding: const EdgeInsets.all(10.0),child: 
-          ElevatedButton(style: style, onPressed: () {}, child: const Text('Next')),
+          ElevatedButton(style: style, onPressed:(){Navigator.pushNamed(context, '/search');} , child: const Text('Next')),
     )]));
   }
 }
---
+++ lib/search.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, annotate_overrides
+
+import 'package:flutter/material.dart';
+
+class Search extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Center(child: Text("Search"))));
+  }
+}
